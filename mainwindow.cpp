a48102b389dc11f0881e858f78330f1255dd7489
g4ySUSmaster <103023776+g4ySUSmaster@users.noreply.github.com>
2022-05-10 09:48:47 +0200

    Licznik

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QDebug>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    licznik =0;
 }
 
 MainWindow::~MainWindow()
@@ -16,6 +17,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    close();
+   ui->label->setText(QString::number(++licznik));
 }
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -10,12 +10,14 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    int licznik;
 };
 #endif // MAINWINDOW_H
